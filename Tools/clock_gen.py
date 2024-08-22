a3f192cf155d07d7b9d00c1f7da568333cfa43e3
Nat-thapas <103213621+Nat-thapas@users.noreply.github.com>
2024-08-22 14:28:40 +0700

    Improve simplification of clock_gen

--- Tools/clock_gen.py
+++ Tools/clock_gen.py
@@ -1,6 +1,5 @@
 from functools import cache
 
-
 DIVIDERS: tuple[int, ...] = (
     2,
     3,
@@ -42,7 +41,7 @@ def closest_product(arr: tuple[int, ...], target: float) -> tuple[list[int], int
         distance_to_one = abs(1 - target)
         distance_to_first = abs(arr[0] - target)
         if distance_to_one < distance_to_first:
-            return [1], 1
+            return [], 1
         else:
             return [arr[0]], arr[0]
     closest = -1
@@ -57,26 +56,38 @@ def closest_product(arr: tuple[int, ...], target: float) -> tuple[list[int], int
             closest = result_product * num
             numbers = result_list + [num]
         if current_distance == new_distance:
-            if len(result_list) < len(numbers):
+            if len(result_list) + 1 <= len(numbers):
                 closest = result_product * num
                 numbers = result_list + [num]
     return numbers, closest
 
 
+def refactor(arr: tuple[int, ...], num: int) -> list[int]:
+    output: list[int] = []
+    index = len(arr) - 1
+    while num > 1:
+        cur = arr[index]
+        while num % cur == 0:
+            num /= cur
+            output.append(cur)
+        index -= 1
+    output.reverse()
+    return output
+
+
 def main():
     target_frequency = eval(input("Enter target frequency: "))
     target_division = 20_000_000 / target_frequency
-    result_numbers, result_product = closest_product(DIVIDERS, target_division)
-    closest_frequency = 20_000_000 / result_product
-    result_numbers.sort()
-    result_numbers = [num for num in result_numbers if num != 1]
+    _, result_product = closest_product(DIVIDERS, target_division)
+    refactored_numbers = refactor(DIVIDERS, result_product)
+    result_frequency = 20_000_000 / result_product
     print(
-        f"Dividers: {' x '.join((f'{num:,}' for num in result_numbers))} = {result_product:,}"
+        f"Dividers: {' x '.join((f'{num:,}' for num in refactored_numbers))} = {result_product:,}"
     )
     print(f"Target frequency: {target_frequency:,.6g} Hz")
-    print(f"Closest frequency: {closest_frequency:,.6g} Hz")
+    print(f"Result frequency: {result_frequency:,.6g} Hz")
     print(
-        f"Error: {abs(target_frequency - closest_frequency) / target_frequency * 100:.3g}%"
+        f"Error: {abs(target_frequency - result_frequency) / target_frequency * 100:.3g}%"
     )
 
 
